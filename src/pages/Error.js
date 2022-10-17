89dc56f677e78917fdad73fb8e2a85c4b63af231
Zoubeiri <housna.zoubeiri@gmail.com>
2022-10-17 20:24:46 +0200

    Error page CSS modifications

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
+import '../styles/Error.css'
+
 function Error() {
   return (
-    <div>
-        <p>Error</p>
+    <div className='kasa-error'>
+      <h1 className='error-title'>404</h1>
+      <span className='error-description'>Oups! La page que vous demandez n'existe pas.</span>
+      <Link to='/' className='error-link'>Retourner sur la page d’accueil</Link>
     </div>
   )
  } 
