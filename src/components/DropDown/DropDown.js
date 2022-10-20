5cc14da497cf1f29ae16a68a2ddb1dce3632c6e4
Zoubeiri <housna.zoubeiri@gmail.com>
2022-10-20 21:49:27 +0200

    Fixing DropDown component

--- src/components/DropDown/DropDown.js
+++ src/components/DropDown/DropDown.js
@@ -1,21 +1,20 @@
 import {useState} from 'react';
 import './DropDown.css';
-import ArrowUp from '../../assets/ArrowUp.png';
-import ArrowDown from '../../assets/ArrowDown.png'
+import Arrow from '../../assets/ArrowDown.png';
 
 function DropDown({ title, description }) {
-  const [isOpen, setIsOpen] = useState(false);
-
-  return isOpen ? (
+    const [isOpen, setIsOpen] = useState(false);
+   
+    return isOpen ? (
     <div className='kasa-dropdown' id={`dropdown-${title}`}>
         <div className='dropdown-header'>
             <div className='dropdown-title'>{title}</div>
             <a
-                className='dropdown-arrow'
+                className={`dropdown-arrow ${isOpen}`}
                 href={`#dropdown-${title}`}
                 onClick={() => setIsOpen(false) }
             >
-                <img src={ArrowUp} alt='dropdown open' />
+                <img src={Arrow} alt='dropdown open' />
             </a>
         </div>
         <div className='dropdown-description'>{description}</div>
@@ -25,15 +24,32 @@ function DropDown({ title, description }) {
         <div className='dropdown-header'>
             <div className='dropdown-title'>{title}</div>
             <a
-                className='dropdown-arrow'
+                className={`dropdown-arrow ${isOpen}`}
                 href={`#dropdown-${title}`}
                 onClick={() => setIsOpen(true)}
             >
-                <img src={ArrowDown} alt='dropdown close' />
+                <img src={Arrow} alt='dropdown close' />
             </a>
         </div>
     </div>
     );
+
+
+    /*return (
+        <div className='kasa-dropdown' id={`dropdown-${title}`}>
+            <div className='dropdown-header'>
+                <div className='dropdown-title'>{title}</div>
+                <a
+                    className={`dropdown-arrow ${isOpen}`}
+                    href={`#dropdown-${title}`}
+                    onClick={() => setIsOpen(true)}
+                >
+                    <img src={Arrow} alt='dropdown open' />
+                </a>
+            </div>
+            { isOpen && <div className='dropdown-description'>{description}</div>}
+        </div>
+    );*/
 }
 
 export default DropDown;
