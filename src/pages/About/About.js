6c89bc9c0f6547bc6e77f34ffae5aacbbed11d9b
Zoubeiri <housna.zoubeiri@gmail.com>
2022-10-26 11:51:50 +0200

    Banner CSS modifications and cleaning code

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -18,11 +18,11 @@ function About() {
         />
         <DropDown
           title="Service"
-          description="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. Nhésitez pas à nous contacter si vous avez la moindre question."
+          description="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
         />
         <DropDown
           title="Responsabilité"
-          description="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à lhôte quau locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+          description="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
         />
       </div>
     </div>
